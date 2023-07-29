e03117dc43831611975e75cad932fa0bffacd865
ravipetla97 <raviatjnv@gmail.com>
2023-07-29 12:45:23 +0530

    rebase merging again conflicts resolved

--- calc.sh
+++ calc.sh
@@ -6,7 +6,16 @@ updates coming sooon.....
 
 int c = a+b;
 
+<<<<<<< HEAD
 3)multiplication
+
 
 rff-rf---r--
 int c= a*b ####this is to demonstrate git rebase
+=======
+
+
+2)substraction
+
+int c =a-b### merging
+>>>>>>> b9807ae ( sub  feature got added)
